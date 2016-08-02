ed74fe01103663ced96d1a3b60354ab7edcda260
Michael Gibbons <migibbons@BELC02MR5T2F6T6.sea.corp.expecn.com>
2016-08-02 09:31:27 -0700

    Corrected quotes in DR main.tf

--- environments/dr/main.tf
+++ environments/dr/main.tf
@@ -4,7 +4,7 @@ module "vault" {
   environment_name = "dr"
   vault-url = "10.205.245.11"
   servers = 2
-  vault_servers = "10.205.245.11,10.205.245.13""
+  vault_servers = "10.205.245.11,10.205.245.13"
   consul_server = "10.205.245.9"
   user = "vault"
   key_path = "vault.key"
