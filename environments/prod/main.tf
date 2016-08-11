145be1a35804931feaf42635184df81ce1a1c8d7
gburgaud <gburgaud@egencia.com>
2016-08-11 18:28:52 +0200

    Add prod Vault variables

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,20 +1,11 @@
 module "vault" {
-  source = "../../../modules/vault/"
-  region = "us-west-2"
+  source = "../../modules/vault/"
+  region = "prod"
   environment_name = "prod"
-  vault-url = "https://vault.infra.us-west2egeprod.local"
-
-  elb_ssl_certificate_id = "arn:aws:iam::059453199592:server-certificate/vault.us-west2.egeprod.local"
-  aws_route_53_zone_id = "Z37WXJWAMCYTTW"
-
-  core_remote_state_bucket_key = "/infra/prod/us-west-2.tfstate"
-  core_remote_state_bucket_name = "egencia-tf-prod"
-
-  consul_remote_state_bucket_key = "/infra/prod/consul/us-west-2.tfstate"
-  consul_remote_state_bucket_name = "egencia-tf-prod"
-
-  key_name = "us-west-2-prod"
-  key_path = "~/.ssh/us-west-2-prod_decrypted"
-
-  account_ids = "059453199592"
+  vault-url = "10.178.169.177"
+  servers = 2
+  vault_servers = "10.178.169.177,10.178.169.176"
+  consul_server = "10.178.169.174"
+  user = "vault"
+  key_path = "vault.key"
 }
