f532525eb1eb8f6a3f18d5c2d463e465c18ff942
gburgaud <gburgaud@egencia.com>
2016-08-30 16:24:37 +0200

    Vault config update

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -2,7 +2,7 @@ module "vault" {
   source = "../../modules/vault/"
   region = "prod"
   environment_name = "prod"
-  vault-url = "http://localhost"
+  vault-url = "10.178.169.177"
   servers = 2
   vault_servers = "10.178.169.177,10.178.169.176"
   consul_server = "10.178.169.174"
